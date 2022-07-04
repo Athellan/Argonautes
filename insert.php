7cd006b6023cf9110139d44d78d80be1b19ad312
Jean-Baptiste CAMBILLARD <jeanbaptiste.cambillard@gmail.com>
2022-07-04 12:19:43 +0200

    V1 CSS a revoir

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+
+// 1.j'appelle pdo
+include('pdo.php');
+
+// 2. je récupère les données POST de mon formulaire
+$name = $_POST['name'];
+
+// 3. j'insère ces données en base
+// Ecriture de la requête INSERT INTO
+$sql = "INSERT INTO argonautes (name) VALUES (:name)";
+
+// Execution de la requête d'insertion (exec, pas query)
+$pdoStatement = $newBD->prepare($sql);
+$pdoStatement->bindValue(':name', $name, PDO::PARAM_STR);
+$pdoStatement->execute();
+
+$rowCount = $pdoStatement->rowCount();
+
+if ($rowCount > 0) {
+    header('location: http://perso/WCS/');
+} else {
+    echo 'Une erreur s\'est produite';
+}
+
+// -----------------------------------------------------
---
+++ select.php
@@ -0,0 +1,19 @@
+<?php
+
+//* version API
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+include('pdo.php');
+
+// script appel BDD
+$sql = "SELECT * 
+FROM `argonautes` 
+ORDER BY name";
+
+$pdoStatement = $newBD->query($sql);
+$results = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+
+$json = json_encode($results);
+
+echo $json;
